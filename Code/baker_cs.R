f461ee1fd35608b84e834159d329862e4c791aa5
Scott Cunningham <scunning@gmail.com>
2022-04-09 11:15:59 -0500

    Code

--- Code/baker_cs.R
+++ Code/baker_cs.R
@@ -16,13 +16,13 @@ baker$treat_date[is.na(baker$treat_date)] <- 0 # untreated units have effective 
 
 # Estimating the effect on y2 (constant) and y (dynamic treatment)
 atts <- att_gt(yname = "y", # LHS variable
-               tname = "year", # time variable
-               idname = "id", # id variable
+               tname = "year", # panel time variable
+               idname = "id", # firms' panel id variable
                gname = "treat_date", # first treatment period variable
                data = baker, # data
                xformla = NULL, # no covariates
                #xformla = ~ l_police, # with covariates
-               est_method = "dr", # "dr" is doubly robust. "ipw" is inverse probability weighting. "reg" is regression
+               est_method = "ipw", # "dr" is doubly robust. "ipw" is inverse probability weighting. "reg" is regression
                control_group = "notyettreated", # set the comparison group which is either "nevertreated" or "notyettreated" 
                bstrap = TRUE, # if TRUE compute bootstrapped SE
                biters = 1000, # number of bootstrap iterations
